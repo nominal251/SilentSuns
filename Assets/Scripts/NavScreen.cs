1dad766ee08ce2096fff3b9101805d787c470d0d
nominal251 <jackhardy524@gmail.com>
2026-09-17 16:26:58 -0400

    Establish screen system and start NAV component

---
+++ Assets/Scripts/NavScreen.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NavScreen : MonoBehaviour
+{
+    public Transform horizontalLine;
+    public Transform verticalLine;
+    public Transform dot;
+
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        if (Keyboard.current == null)
+            return;
+
+        // horizontal line up/down
+        if (Keyboard.current.upArrowKey.isPressed)
+            horizontalLine.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        if (Keyboard.current.downArrowKey.isPressed)
+            horizontalLine.position += Vector3.down * moveSpeed * Time.deltaTime;
+
+        // vertical line left/right
+        if (Keyboard.current.leftArrowKey.isPressed)
+            verticalLine.position += Vector3.left * moveSpeed * Time.deltaTime;
+
+        if (Keyboard.current.rightArrowKey.isPressed)
+            verticalLine.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+        // central dot position (unused for now)
+        /*dot.position = new Vector3(
+            verticalLine.position.x,
+            horizontalLine.position.y,
+            dot.position.z
+        );*/
+    }
+}
