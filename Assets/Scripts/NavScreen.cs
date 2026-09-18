afb98589423ee3286e3fa54eb4c4f7fa000a335e
nominal251 <jackhardy524@gmail.com>
2026-09-18 14:56:22 -0400

    remove sprites

--- Assets/Scripts/NavScreen.cs
+++ Assets/Scripts/NavScreen.cs
@@ -15,7 +15,11 @@ public class NavScreen : MonoBehaviour
     public Button upButton;
     public Button downButton;
 
-    public float moveSpeed = 5f;
+    public Button startButton;
+    public Button stopButton;
+
+    public float reticleMoveSpeed = 5f;
+    public float shipMoveSpeed = 1f;
 
     void Update()
     {
@@ -24,17 +28,17 @@ public class NavScreen : MonoBehaviour
 
         // horizontal line up/down
         if (upButton.activated)
-            horizontalLine.position += Vector3.up * moveSpeed * Time.deltaTime;
+            horizontalLine.position += Vector3.up * reticleMoveSpeed * Time.deltaTime;
 
         if (downButton.activated)
-            horizontalLine.position += Vector3.down * moveSpeed * Time.deltaTime;
+            horizontalLine.position += Vector3.down * reticleMoveSpeed * Time.deltaTime;
 
         // vertical line left/right
         if (leftButton.activated)
-            verticalLine.position += Vector3.left * moveSpeed * Time.deltaTime;
+            verticalLine.position += Vector3.left * reticleMoveSpeed * Time.deltaTime;
 
         if (rightButton.activated)
-            verticalLine.position += Vector3.right * moveSpeed * Time.deltaTime;
+            verticalLine.position += Vector3.right * reticleMoveSpeed * Time.deltaTime;
 
         // central dot position
         reticleCenter.position = new Vector3(
@@ -42,5 +46,28 @@ public class NavScreen : MonoBehaviour
             horizontalLine.position.y,
             reticleCenter.position.z
         );
+
+        if (startButton.activated)
+
+        if (isMoving)
+        {
+            Vector3 targetPosition = new Vector3(
+                reticleCenter.position.x,
+                reticleCenter.position.y,
+                shipPos.position.z
+            );
+
+            shipPos.position = Vector3.MoveTowards(
+                shipPos.position,
+                targetPosition,
+                shipMoveSpeed * Time.deltaTime
+            );
+
+            if (Mathf.Approximately(shipPos.position.x, targetPosition.x) &&
+                Mathf.Approximately(shipPos.position.y, targetPosition.y))
+            {
+                isMoving = false;
+            }
+        }
     }
 }
