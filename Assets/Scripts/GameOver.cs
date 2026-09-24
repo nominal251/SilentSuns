315809236672be448c150fc4215751bee9133696
nominal251 <jackhardy524@gmail.com>
2026-09-24 01:37:48 -0400

    Update lighting and implement game over

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject lights;
+
+    public AudioSource audioSource;
+    public AudioClip gameOverAudio;
+
+    public string sceneToLoad;
+
+    public float positionTolerance = 0.5f;
+
+    private bool gameOverStarted = false;
+
+    void Update()
+    {
+        if (!gameOverStarted && Vector3.Distance(transform.position, enemy.transform.position) <= positionTolerance)
+        {
+            gameOverStarted = true;
+            StartCoroutine(GameOverSequence());
+        }
+    }
+
+    IEnumerator GameOverSequence()
+    {
+        lights.SetActive(false);
+
+        audioSource.PlayOneShot(gameOverAudio);
+
+        yield return new WaitForSeconds(2.5f);
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
